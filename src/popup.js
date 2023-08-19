e34e22b8c3783d3813810a49222bf69e7eb08d52
metapox <84276752+metapox@users.noreply.github.com>
2023-08-19 23:13:43 +0900

    change link style

--- src/popup.js
+++ src/popup.js
@@ -2,14 +2,25 @@ function createTraceLink(toolUrl) {
   chrome.storage.local.get(function(data) {
     const traces = data['traceData'] || [];
     let linksDiv = document.getElementById('links');
+    console.log(linksDiv);
 
     traces.forEach(function(trace) {
+      const tr = document.createElement('tr');
+      tr.classList.add("collection-item");
+      const uritd = document.createElement('td');
+      uritd.textContent = trace.url;
+
+      const traceparenttd = document.createElement('td');
       let a = document.createElement('a');
       a.href = toolUrl.replace('${traceparent}', trace.headerValue.traceId); // Assuming each traceparent is a URL
       a.classList.add("collection-item");
-      a.textContent = trace.url;
+      a.textContent = 'prod';
       a.target = "_blank";
-      linksDiv.appendChild(a);
+      traceparenttd.appendChild(a);
+      tr.appendChild(uritd);
+      tr.appendChild(traceparenttd);
+
+      linksDiv.appendChild(tr);
     });
   });
 }
