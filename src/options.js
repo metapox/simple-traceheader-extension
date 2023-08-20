6eec1c98000aecb2e4cf2e48d5202f12249bdf35
metapox <84276752+metapox@users.noreply.github.com>
2023-08-20 11:59:21 +0900

    faet: set dev link

--- src/options.js
+++ src/options.js
@@ -1,11 +1,23 @@
-const url = document.getElementById('url-label');
-chrome.storage.sync.get('toolUrl', function(data) {
-  url.textContent = `URL: ${data['toolUrl']}`;
+const prodUrl = document.getElementById('prod-url-label');
+chrome.storage.sync.get('toolUrl1', function(data) {
+  prodUrl.textContent = `URL: ${data['toolUrl1']}`;
 });
 
-document.getElementById('save').addEventListener('click', function() {
-  const url = document.getElementById('url').value;
-  chrome.storage.sync.set({'toolUrl': url}, function() {
+document.getElementById('prod-save').addEventListener('click', function() {
+  const url = document.getElementById('prod-url').value;
+  chrome.storage.sync.set({'toolUrl1': url}, function() {
+    console.log('URL is ' + url);
+  });
+});
+
+const devUrl = document.getElementById('dev-url-label');
+chrome.storage.sync.get('toolUrl2', function(data) {
+  devUrl.textContent = `URL: ${data['toolUrl2']}`;
+});
+
+document.getElementById('dev-save').addEventListener('click', function() {
+  const url = document.getElementById('dev-url').value;
+  chrome.storage.sync.set({'toolUrl2': url}, function() {
     console.log('URL is ' + url);
   });
 });
