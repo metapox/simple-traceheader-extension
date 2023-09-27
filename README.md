bd5b3158a092386f20e63a785d2a3a6281106438
minami-takuya <minamitakuya@lifull.com>
2023-09-27 11:44:52 +0900

    fix readme

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # simple-traceheader-extension
+
+OpenTelemtryのtraceparentを自動で付与して、
+
+grafana tempoやjeagerなどの可視化ツールのurlを設定しておくと
+
+そのtraceを表示するためのリンクを生成することができるchrome拡張です。
--- src/options.js
+++ src/options.js
@@ -1,23 +1,25 @@
 const prodUrl = document.getElementById('prod-url-label');
 chrome.storage.sync.get('toolUrl1', function(data) {
-  prodUrl.textContent = `URL: ${data['toolUrl1']}`;
+  prodUrl.textContent = `prod環境のURL: ${data['toolUrl1']}`;
 });
 
 document.getElementById('prod-save').addEventListener('click', function() {
   const url = document.getElementById('prod-url').value;
+
   chrome.storage.sync.set({'toolUrl1': url}, function() {
-    console.log('URL is ' + url);
+    prodUrl.textContent = `prod環境のURL: ${data['toolUrl1']}`;
   });
 });
 
 const devUrl = document.getElementById('dev-url-label');
 chrome.storage.sync.get('toolUrl2', function(data) {
-  devUrl.textContent = `URL: ${data['toolUrl2']}`;
+  devUrl.textContent = `dev環境のURL: ${data['toolUrl2']}`;
 });
 
 document.getElementById('dev-save').addEventListener('click', function() {
   const url = document.getElementById('dev-url').value;
+
   chrome.storage.sync.set({'toolUrl2': url}, function() {
-    console.log('URL is ' + url);
+    devUrl.textContent = `dev環境のURL: ${data['toolUrl2']}`;
   });
 });
